6fcbca1d81657cc1d58410e2b92343ca5e94f775
Yesulen Tsedensodnom <sodesulen@gmail.com>
2024-04-13 21:15:03 +0100

    fixed to use recursion

--- main.py
+++ main.py
@@ -1,17 +1,13 @@
-# define function, we are using recursion
-def eaclidean_algorithm(n1, n2):
-    # find the min
-    gcd = min(n1, n2)
-    # implement recursion
-    while gcd:
-        # base case is when there is no remainder when divided by 2 numbers
-        if n1 % gcd == 0 and n2 % gcd == 0:
-            break
-        # minus one if have not found the base case, repeat until found
-        gcd -= 1
-    # return the found gcd
-    return gcd
+# define the function using recursion
+def euclidean_algorithm(n1, n2):
+    # base case is when n2 becomes 0, assuming n1 will always be greater
+    if n2 == 0:
+        # when n2 becomes 0, n1 is the gcd
+        return n1
+    else:
+        # repeat with n2 and the remainder of n1 divided by the n2
+        return euclidean_algorithm(n2, n1 % n2)
 
-# give values, and print result
-gcd = eaclidean_algorithm(39, 120)
+# provide values and print the result
+gcd = euclidean_algorithm(39, 120)
 print("The GCD is", gcd)
