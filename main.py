7edbfe5e39c14cd23de68597f05cd46eafdf4636
Yesulen Tsedensodnom <sodesulen@gmail.com>
2024-04-13 21:43:56 +0100

    suggestion for more extension and its pseudocode

--- main.py
+++ main.py
@@ -41,3 +41,33 @@ if __name__ == "__main__":
     except ValueError as ve:
         # handle invalid input error
         print("Error:", ve)
+
+
+
+# another extension could be to state all the prime divisors
+# of the gcd, which could provide more info abt the numbers.
+
+# the pseudocode would be :
+
+# Function PrimeFactorsOfGCD(a, b)
+#     gcd = EuclideanAlgorithm(a, b)  // Compute the GCD using the standard Euclidean Algorithm
+#
+#     // Initialize an empty list to store the prime factors of the GCD
+#     prime_factors = []
+#
+#     // Iterate from 2 to the square root of the GCD
+#     for i from 2 to floor(sqrt(gcd))
+#         // Check if i is a factor of the GCD
+#         while gcd % i == 0
+#             // If i is a factor, add it to the list of prime factors
+#             prime_factors.append(i)
+#             // Divide the GCD by i
+#             gcd = gcd / i
+#
+#     // If the GCD is still greater than 1, it is a prime factor itself
+#     if gcd > 1
+#         prime_factors.append(gcd)
+#
+#     // Return the list of prime factors of the GCD
+#     return prime_factors
+# End Function
